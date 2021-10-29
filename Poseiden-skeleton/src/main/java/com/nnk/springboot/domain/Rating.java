88c9e9487723225d6fd2cd97cdb0ae1549cc171e
Marie <marie@exemple.fr>
2021-10-29 13:38:53 +0200

    begin project : fill domain class

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,12 +1,26 @@
 package com.nnk.springboot.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
+    
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private Integer id;
+	
+	private String moodysRating;
+	private String sandPRating;
+	private String fitchRating;
+	private Integer orderNumber;
+	
 }
