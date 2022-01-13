14373431baf4cd18d8593bf9bf3ad9e099139cbe
Marie <marie@exemple.fr>
2022-01-13 19:22:59 +0100

    deleting useless files

--- Poseiden-skeleton/target/classes/import.sql
+++
@@ -1,4 +0,0 @@
-insert into users(id, fullname, username, password, role) values(nextval('users_id_seq'), 'Administrator', 'admin', '$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa', 'ADMIN');
-insert into users(id, fullname, username, password, role) values(nextval('users_id_seq'), 'User', 'user', '$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa', 'USER');
-insert into users(id, fullname, username, password, role) values(nextval('users_id_seq'), 'User2', 'user2', '$2a$10$a0hlNqCasHViWB8VkJrjj./z1OgM9WShM6B6BJNOwCJuQJ4sSS9gm', 'USER');
-insert into users(id, fullname, username, password, role) values(nextval('users_id_seq'), 'Administrator2', 'admin2', '$2a$10$a0hlNqCasHViWB8VkJrjj./z1OgM9WShM6B6BJNOwCJuQJ4sSS9gm', 'ADMIN');
